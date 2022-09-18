ac8bae29de50cfa693b7862e112ec76e53ee2c35
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2022-09-18 18:11:30 -0500

    Dependencies Installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'cms-admin-dash',
+    appId: '1:430618933811:web:36dc7f2e99774ef82c8146',
+    storageBucket: 'cms-admin-dash.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAMojBZkWbAZRY9RfVSnQwxsr_vgc9H7TY',
+    authDomain: 'cms-admin-dash.firebaseapp.com',
+    messagingSenderId: '430618933811',
+    measurementId: 'G-4WRRXV68YB',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'cms-admin-dash',
+    appId: '1:430618933811:web:36dc7f2e99774ef82c8146',
+    storageBucket: 'cms-admin-dash.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAMojBZkWbAZRY9RfVSnQwxsr_vgc9H7TY',
+    authDomain: 'cms-admin-dash.firebaseapp.com',
+    messagingSenderId: '430618933811',
+    measurementId: 'G-4WRRXV68YB',
+  },
   production: false
 };
 
