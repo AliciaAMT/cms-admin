6c1715ce325947c1f0446a065bdae02cc9ea6a4d
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2022-09-21 16:34:28 -0500

    Finish nav components later

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from 'src/app/components/home/home.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
+  component = HomeComponent;
 
   constructor() { }
 
